74f7a5d5a2f38048ae9e5716bdc06f44c1845320
DanteZzzy <gabrieltf04@gmail.com>
2026-06-08 19:32:43 -0300

    Caminho static favicon

--- servico_agendamentos/agendamento_system/urls.py
+++ servico_agendamentos/agendamento_system/urls.py
@@ -1,9 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.shortcuts import redirect
+from django.contrib.staticfiles.storage import staticfiles_storage
+from django.views.generic.base import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('agendar/', include('agendamentos.urls')),
     path('', lambda request: redirect('agendar/')),
+    path('favicon.ico', RedirectView.as_view(url=staticfiles_storage.url('agendamentos/favicon.ico'))),
 ]
